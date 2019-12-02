d1a1592ad4001feef86edacd9c3eaeedf4e10225
Ron Borysowski <roni537@gmail.com>
2019-12-02 17:56:49 +0200

    chore: Change configuration

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const config = {
     server: {
-        port: 3000,
+        port: 80,
     },
     db: {
         connectionString: `mongodb://${process.env.DB_SERVERS || 'localhost:27017'}/${process.env.DB_NAME || 'approval-system'}${process.env.DB_REPLICA_NAME ? `?replicaSet=${process.env.DB_REPLICA_NAME}` : ''}`,
     },
     auth: {
-        callbackURL: '',
-        shragaURL: '',
-        useEnrichId: false,
+        callbackURL: 'http://localhost/auth/callback',
+        shragaURL: 'http://localhost:3000',
+        useEnrichId: true,
         secret: 'ApPr0vaL_5ySt3m'
     },
     defaultUnitName: 'default'
