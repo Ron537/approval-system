9ef890eb38e70c6caac57f69853666774074e8c0
Ron Borysowski <roni537@gmail.com>
2019-12-03 17:05:43 +0200

    refactor: add user router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { RequestRouter } from "./request/request.router";
 import { UnitRouter } from './unit/unit.router';
+import { UserRouter } from "./user/user.router";
+import { AuthenticationMiddleware } from "./authentication/middleware";
 
 const AppRouter: Router = Router();
 
 AppRouter.use('/request', RequestRouter);
-AppRouter.use('/unit', UnitRouter);
+AppRouter.use('/unit', AuthenticationMiddleware.requireAuth, UnitRouter);
+AppRouter.use('/user', AuthenticationMiddleware.requireAuth, UserRouter);
+
 
 export { AppRouter };
