040d4c4655b328b3967c15c9d9452a212b9424d6
Ron Borysowski <roni537@gmail.com>
2019-12-03 15:10:17 +0200

    chore: take config from env

--- src/config.ts
+++ src/config.ts
@@ -4,35 +4,31 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0 as any;
 
 export const config = {
     server: {
-        port: 80,
+        port: process.env.PORT || 80,
     },
     db: {
         connectionString: `mongodb://${process.env.DB_SERVERS || 'localhost:27017'}/${process.env.DB_NAME || 'approval-system'}${process.env.DB_REPLICA_NAME ? `?replicaSet=${process.env.DB_REPLICA_NAME}` : ''}`,
     },
     auth: {
-        callbackURL: 'http://localhost/auth/callback',
-        shragaURL: 'http://localhost:3000',
+        callbackURL: process.env.AUTH_CALLBACK_URL || 'http://localhost/auth/callback',
+        shragaURL: process.env.SHRAGA_URL || 'http://localhost:3000',
         useEnrichId: true,
         secret: 'ApPr0vaL_5ySt3m',
         daysExpires: 3,
     },
     defaultUnitName: 'default',
-    userService: {
-        endpoint: 'http://localhost/us',
-        token: 'TOKEN'
-    },
     spike: {
         options: {
-            redisHost: 'redis://localhost',
-            ClientId: 'VVFG4sMePMkyMVXo9Z_a8NvTv2lwWyyEqhNwpjii',
-            ClientSecret: 'esDNkyivgAUQexvUbhintFC0UVnUIYc1wc49TtG4XbhvsXh5CkVO_nxtcM6dp_4dqejBLGWQTEDHABjQBO28vx3TV~EkWuRl0e1S',
-            spikeURL: 'https://51.144.178.121:1337/oauth2/token',
-            tokenRedisKeyName: 'token',
-            spikePublicKeyFullPath: path.resolve(__dirname, '../certificate/certificate.pem'),
+            redisHost: process.env.SPIKE_REDIS_HOST || 'redis://localhost',
+            ClientId: process.env.SPIKE_CLIENT_ID || 'VVFG4sMePMkyMVXo9Z_a8NvTv2lwWyyEqhNwpjii',
+            ClientSecret: process.env.SPIKE_CLIENT_SECRET || 'esDNkyivgAUQexvUbhintFC0UVnUIYc1wc49TtG4XbhvsXh5CkVO_nxtcM6dp_4dqejBLGWQTEDHABjQBO28vx3TV~EkWuRl0e1S',
+            spikeURL: process.env.SPIKE_URL || 'https://51.144.178.121:1337/oauth2/token',
+            tokenRedisKeyName: process.env.SPIKE_REDIS_KEY_NAME || 'token',
+            spikePublicKeyFullPath: process.env.SPIKE_PUBLIC_KEY_FULL_PAYH || path.resolve(__dirname, '../certificate/certificate.pem'),
             useRedis: true,
         }
     },
     kartoffel: {
-        url: 'http://kartoffel-master.eastus.cloudapp.azure.com:3001/api',
+        url: process.env.KARTOFFEL_URL || 'http://kartoffel-master.eastus.cloudapp.azure.com:3001/api',
     }
 }
